0069d88b948870883956f50c6596d78e76e4643f
Yasmin Araujo <52540870+yasmin-araujo@users.noreply.github.com>
2023-12-05 18:11:54 +0100

    Add action to main menu buttons

---
+++ VR Painting/Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    
+    public void SettingsButton()
+    {
+        // SceneManager.LoadSceneAsync(1);
+    }
+}
